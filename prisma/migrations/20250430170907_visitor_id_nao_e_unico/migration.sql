4436cd31eb735062b009f074ac9e42193e5ce9ff
Lucas Camargo <lucascamargo.dev@gmail.com>
2025-05-07 11:11:38 -0300

    Produção

--- prisma/migrations/20250430170907_visitor_id_nao_e_unico/migration.sql
+++
@@ -1,2 +0,0 @@
--- DropIndex
-DROP INDEX "visits_visitor_id_key";
--- prisma/migrations/20250430173051_add_visit_and_contact_count_in_adverts_table/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "adverts" ADD COLUMN     "contact_count" INTEGER NOT NULL DEFAULT 0,
-ADD COLUMN     "view_count" INTEGER NOT NULL DEFAULT 0;
